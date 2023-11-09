6e63781bfb8986d20383eb97338c0ca2a098d730
Eliezer Flores <fuentes133@hotmail.com>
2023-11-09 18:46:00 -0500

    update

---
+++ pybank/main.py
@@ -0,0 +1,54 @@
+import os
+import csv
+
+budget_data_csv = os.path.join("Resources", "budget_data.csv")
+
+# define variables
+TotalMonths = []
+TotalAmount = []
+MonthlyChangeProfit = []
+
+MonthlyChange = 0
+MonthlyChangeTotal = 0
+InitialProfitCounter = 0
+
+# open file to use
+with open(budget_data_csv) as csvfile:
+    csvreader = csv.reader(csvfile, delimiter=",")
+
+# for loop
+    for row in csvreader:
+        TotalMonths.append(row[0])
+        SumMonths = len(TotalMonths)
+        TotalAmount.append(float(row[1]))
+        ProfitForMonth = int(row[1])
+        MonthlyChange = float(ProfitForMonth)
+        MonthlyChangeTotal = MonthlyChangeTotal + MonthlyChange
+        InitialProfitCounter = int(row[1])
+        MonthlyChangeProfit.append(MonthlyChange)
+        MaxProfit = max(MonthlyChangeProfit)
+        MaxIndex = MonthlyChangeProfit.index(MaxProfit)
+        MinProfit = min(MonthlyChangeProfit)
+        MinIndex = MonthlyChangeProfit.index(MinProfit)
+        AvgChangeProfit = round(MonthlyChangeTotal/SumMonths)
+        SumAmount = sum(TotalAmount)
+
+# print to terminal
+print(f'Financial Analysis')
+print(f'------------------')
+print(f'Total Months: {SumMonths}')
+print(f'Total Amount: ${SumAmount}')
+print(f'Average Monthly Change: ${AvgChangeProfit}')
+print(f'Greatest Increase in Profits: {TotalMonths[MaxIndex]} ${MaxProfit}')
+print(f'Greatest Decrease in Profits: {TotalMonths[MinIndex]} ${MinProfit}')
+
+# output to file
+output = open("PyBank/FinancialAnalysis.txt", 'w')
+output.write(f'''
+Financial Analysis
+------------------
+Total Months: {SumMonths}
+Total: ${SumAmount}
+Average Monthly Change: ${AvgChangeProfit}
+Greatest Increase in Profits: {TotalMonths[MaxIndex]} (${MaxProfit})
+Greatest Decrease in Profits: {TotalMonths[MinIndex]} (${MinProfit})''')
